c038ca50d701513291098acc5db167260ddc2afd
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 17:02:41 +0000

    Update AnagramChecker to pass feature test

--- spec/features/WordIsValidAnagramSpec.js
+++ spec/features/WordIsValidAnagramSpec.js
@@ -1,7 +1,7 @@
 describe('Word is a valid anagram', () => {
   const AnagramChecker = require('../../lib/AnagramChecker');
 
-  xit('Checks if a given word, known as the subject, is an anagram, given a file containing a list of words to check against', () => {
+  it('Checks if a given word, known as the subject, is an anagram, given a file containing a list of words to check against', () => {
     const fs = require('fs');
     const subject = 'silent';
     const filePath = './public/anagrams-of-silent.txt';
