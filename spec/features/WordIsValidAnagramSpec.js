99f4ea8f359a6c3e280de33704c64711c18cd1d5
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 14:19:08 +0000

    Add empty feature test

---
+++ spec/features/WordIsValidAnagramSpec.js
@@ -0,0 +1,5 @@
+describe('Word is a valid anagram', () => {
+  it('Checks if a given word (known as the subject) is an anagram, given a file containing a list of words to check against', () => {
+    
+  });
+});
