79c1d2e0c6f2cde4bb75b7fcfb30116a9df485d8
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 14:58:29 +0000

    Add pending feature test for acceptance criteria

--- spec/features/WordIsValidAnagramSpec.js
+++ spec/features/WordIsValidAnagramSpec.js
@@ -1,9 +1,11 @@
 describe('Word is a valid anagram', () => {
-  it('Checks if a given word (known as the subject) is an anagram, given a file containing a list of words to check against', () => {
-    const SUBJECT = 'silent';
+  const AnagramChecker = require('../../lib/AnagramChecker');
 
-    require('fs').readFileSync('./public/anagrams-of-silent.txt').toString().split('\n').forEach((word) => {
-      console.log(word);
-    });
+  xit('Checks if a given word, known as the subject, is an anagram, given a file containing a list of words to check against', () => {
+    const SUBJECT = 'silent';
+    const filePath = './public/anagrams-of-silent.txt';
+    const expectedOutput = require('fs').readFileSync(filePath).toString();
+
+    expect(AnagramChecker.check(SUBJECT, filePath)).toEqual(expectedOutput);
   });
 });
