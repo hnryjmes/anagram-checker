a1476c044903fa12c7ed9583c9a8cf3e5b9cc28b
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 17:10:46 +0000

    Add feature test for case when there is no anagram in given list

---
+++ spec/features/WordIsNotValidAnagramSpec.js
@@ -0,0 +1,11 @@
+describe('Word does not have a valid anagram in given list', () => {
+  const AnagramChecker = require('../../lib/AnagramChecker');
+
+  it('Checks if a given word, known as the subject, is an anagram, given a file containing a list of words to check against', () => {
+    const subject = 'silent';
+    const filePath = './public/a.txt';
+    const expectedOutput = '';
+
+    expect(AnagramChecker.check(subject, filePath)).toEqual(expectedOutput);
+  });
+});
