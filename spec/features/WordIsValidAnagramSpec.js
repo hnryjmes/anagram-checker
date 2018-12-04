ef6f5a43c139acb0cd3231e938002f617680dbc4
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 16:25:07 +0000

    Update feature test to use camelCase for constants

--- spec/features/WordIsValidAnagramSpec.js
+++ spec/features/WordIsValidAnagramSpec.js
@@ -2,10 +2,11 @@ describe('Word is a valid anagram', () => {
   const AnagramChecker = require('../../lib/AnagramChecker');
 
   xit('Checks if a given word, known as the subject, is an anagram, given a file containing a list of words to check against', () => {
-    const SUBJECT = 'silent';
-    const FILE_PATH = './public/anagrams-of-silent.txt';
-    const EXPECTED_OUTPUT = require('fs').readFileSync(FILE_PATH).toString();
+    const fs = require('fs');
+    const subject = 'silent';
+    const filePath = './public/anagrams-of-silent.txt';
+    const expectedOutput = fs.readFileSync(filePath).toString();
 
-    expect(AnagramChecker.check(SUBJECT, FILE_PATH)).toEqual(EXPECTED_OUTPUT);
+    expect(AnagramChecker.check(subject, filePath)).toEqual(expectedOutput);
   });
 });
