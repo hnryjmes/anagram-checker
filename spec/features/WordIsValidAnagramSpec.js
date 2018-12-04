ab910a804af36542afb6f367fd035ea3d8e44956
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 14:25:38 +0000

    Add test .txt file in public folder

--- spec/features/WordIsValidAnagramSpec.js
+++ spec/features/WordIsValidAnagramSpec.js
@@ -1,5 +1,9 @@
 describe('Word is a valid anagram', () => {
   it('Checks if a given word (known as the subject) is an anagram, given a file containing a list of words to check against', () => {
-    
+    const SUBJECT = 'silent';
+
+    require('fs').readFileSync('./public/anagrams-of-silent.txt').toString().split('\n').forEach((word) => {
+      console.log(word);
+    });
   });
 });
