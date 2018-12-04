f4aaaf5300c06eef15a3d26e9aaff7ee76db5b80
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 15:25:50 +0000

    Update name of test to reflect change

--- spec/units/AnagramCheckerSpec.js
+++ spec/units/AnagramCheckerSpec.js
@@ -2,7 +2,7 @@ describe('AnagramChecker', () => {
   const AnagramChecker = require('../../lib/AnagramChecker');
 
   describe('.check', () => {
-    it('returns an empty string when inputs are invalid', () => {
+    it('throws an error when number of arguments is not 2', () => {
       expect(() => {
         AnagramChecker.check(); 
       }).toThrow('Error: incorrect number of arguments, expecting 2');
