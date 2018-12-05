29e787be961d4212f72a642c046a1fbf4137ce63
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-05 09:34:26 +0000

    Add passing test for .csv files

--- spec/features/WordIsValidAnagramSpec.js
+++ spec/features/WordIsValidAnagramSpec.js
@@ -1,10 +1,17 @@
-describe('Word has a valid anagram in given list', () => {
+describe('Checks if subject is an anagram and returns matches', () => {
   const AnagramChecker = require('../../lib/AnagramChecker');
+  const fs = require('fs');
+  const subject = 'silent';
 
-  it('Checks if a given word, known as the subject, is an anagram, given a file containing a list of words to check against', () => {
-    const fs = require('fs');
-    const subject = 'silent';
+  it('Word has a valid anagram in given .txt list', () => {
     const filePath = './public/anagrams-of-silent.txt';
+    const expectedOutput = fs.readFileSync(filePath).toString();
+
+    expect(AnagramChecker.check(subject, filePath)).toEqual(expectedOutput);
+  });
+
+  it('Word has a valid anagram in given .csv list', () => {
+    const filePath = './public/anagrams-of-silent.csv';
     const expectedOutput = fs.readFileSync(filePath).toString();
 
     expect(AnagramChecker.check(subject, filePath)).toEqual(expectedOutput);
